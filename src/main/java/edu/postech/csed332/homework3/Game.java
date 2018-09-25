c76568b4079da6b73a629bc0e562c2f38439b153
yuoa <contact@yuoa.pm>
2018-09-26 07:19:51 +0900

    ❤ Basic "Game" class algorithm implemented

--- src/main/java/edu/postech/csed332/homework3/Game.java
+++ src/main/java/edu/postech/csed332/homework3/Game.java
@@ -1,5 +1,9 @@
 package edu.postech.csed332.homework3;
 
+import java.util.Arrays;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Receives a sudoku puzzle as a file by the command line argument, solves the puzzle, and makes a
  * file for each solution
@@ -9,13 +13,40 @@ package edu.postech.csed332.homework3;
  *
  */
 public class Game {
-	// TODO implement this
+	private static Logger log = LoggerFactory.getLogger(Game.class);
 
 	/**
 	 * @param args name of the input file for a sudoku puzzle
 	 */
 	public static void main(String[] args) {
-		// TODO implement this
+		
+		log.info("SUDOKUS started with {} problem(s)", args.length);
+
+		// Iterate args, load game, and solve the game
+		for (String prob : args) {
+
+			log.info("New game: from {}", prob);
+
+			try {
+
+				Sudoku game = new Sudoku(prob);
+
+			} catch (Exception e) {
+
+				log.error(e.toString());
+
+			}
+
+		}
+
+		log.info("SUDOKUS iterating finished.");
+
+	}
+
+	public static class WrongGameException extends Exception {
+
+		private static final long serialVersionUID = -3590540730484860628L;
+
 	}
 
 }
